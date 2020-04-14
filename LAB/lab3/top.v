730642dfd7cb444e76ca42053517cb9cf3adef44
Kevin Chen <kevin51034@gmail.com>
2020-04-15 00:20:54 +0800

    lab3 hw with button

--- LAB/lab3/top.v
+++ LAB/lab3/top.v
@@ -3,18 +3,29 @@ module top(
     input CLK100MHZ,//clk
     input [15:0]SW,
     input BTNC,
-    output [5:0]LED
+    output reg [5:0]LED
     );
-    //wire [3:0] a, b, c, d;
-    //wire [5:0] sum;
-    // assign a = SW[3:0];
-    // assign b = SW[7:4];
-    // assign c = SW[11:8];
-    // assign d = SW[15:12];
-    // assign sum = a + b + c + d;
-    
-    /*****your design******/
-    
+        //reg LED
 
+    /*****your design******/
+     always@(posedge CLK100MHZ)
+	 begin
+		if(BTNC == 1'b1)
+		//always@(BTNC)
+		begin
+			//wire [3:0] a, b, c, d;
+			//wire [5:0] sum;
+			//assign a = SW[3:0];
+			//assign b = SW[7:4];
+			//assign c = SW[11:8];
+			//assign d = SW[15:12];
+		     LED <= SW[3:0] + SW[7:4] + SW[11:8] + SW[15:12];
+			//assign LED = sum;
+		end
+		else
+		begin
+		     LED <= LED;
+		end
+	end
     /**********************/
 endmodule
